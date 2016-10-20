b29ae0e0a6799c9dbc9cef44246fe1ed9237a4c2
cuijiabin <cuijia_bin@126.com>
2016-10-21 00:51:45 +0800

    问题修正

--- main.go
+++ main.go
@@ -16,13 +16,6 @@ import (
 
 //文件块拆分
 const fileChunk = 512 * 1024 * 1024 // 512M
-//统计需要遍历的文件夹下面的数量
-var fileCount = 0
-var rootPath string
-var resultPath string
-
-//默认匹配所有文件
-var regStr = "(.*)"
 
 //返回结果
 type result struct {
@@ -46,42 +39,47 @@ func byteToHex(data []byte) string {
 
 func main() {
 
-	argNum := len(os.Args)
-	rootPath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
-	rootPath = strings.Replace(rootPath, "\\", "/", -1)
-	resultPath = rootPath + "result.txt"
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	dir = strings.Replace(dir, "\\\\", "/", -1)
+	dir = strings.Replace(dir, "\\", "/", -1)
+	var resultName = dir + "/result.txt"
+	var regStr = "(.*)"
 
-	if argNum >= 4 {
-		rootPath = os.Args[1]
-		resultPath = os.Args[2]
-		regStr = os.Args[3]
-	} else if argNum == 3 {
-		rootPath = os.Args[1]
-		resultPath = os.Args[2]
-	} else if argNum == 2 {
-		rootPath = os.Args[1]
+	fmt.Println("请输入分析目录，结果文件，过滤正则，用空格分开；默认为当前目录")
+	fmt.Scanln(&dir, &resultName, &regStr)
+	_, err := os.Stat(dir)
+	if err != nil && err.Error() == os.ErrNotExist.Error() {
+		fmt.Println("分析目录不正确")
+		return
 	}
+	resultName = strings.Replace(resultName, "\\\\", "/", -1)
+	resultName = strings.Replace(resultName, "\\", "/", -1)
 
-	CountFile(rootPath)
-	fmt.Println("文件数量：", fileCount)
+	fmt.Printf("遍历目录 %s; 结果文件为 %s; 过滤正则 %s\n", dir, resultName, regStr)
+	WalkAanlyseFile(dir, resultName, regStr)
 
-	f, err := os.Create(resultPath)
+}
+
+func WalkAanlyseFile(dir string, resultName string, regStr string) {
+
+	f, err := os.Create(resultName)
 	if err != nil {
-		panic(err)
+		fmt.Println("结果文件不合法")
+		return
 	}
 	defer f.Close()
 	w := bufio.NewWriter(f)
 
 	c := make(chan string, 20)
-	go GetFilePath(rootPath, c)
+	go GetFilePath(dir, c, resultName, regStr)
 
-	tick := time.Tick(1e8)
-	readCount := 0
+	tick := time.Tick(3 * time.Second)
+
 	for {
 		select {
 		case <-tick:
 			w.Flush()
-			if readCount >= fileCount {
+			if len(c) < 1 {
 				fmt.Println("文件统计结束")
 				return
 			}
@@ -92,13 +90,11 @@ func main() {
 			if _, err := w.WriteString(context + "\r\n"); err != nil {
 				return
 			}
-			readCount += 1
 		}
 	}
 }
 
-//统计文件夹下文件数量
-func CountFile(root string) {
+func GetFilePath(root string, c chan string, resultName string, regStr string) {
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -106,26 +102,7 @@ func CountFile(root string) {
 		if !info.Mode().IsRegular() {
 			return nil
 		}
-		if strings.EqualFold(path, resultPath) {
-			return nil
-		}
-		match, _ := regexp.MatchString(regStr, info.Name())
-		if match {
-			fileCount++
-		}
-		return nil
-	})
-}
-
-func GetFilePath(root string, c chan string) {
-	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.Mode().IsRegular() {
-			return nil
-		}
-		if strings.EqualFold(path, resultPath) {
+		if strings.EqualFold(path, resultName) {
 			return nil
 		}
 		match, _ := regexp.MatchString(regStr, info.Name())
